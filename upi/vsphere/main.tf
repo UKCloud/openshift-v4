b51087a53d931b3e6bfedab6d8c1471948a1fa7a
Gareth Ellner <gellner@ukcloud.com>
2019-07-31 15:34:30 +0100

    Code fixes discovered in Spike (except hardcoded IPs...

--- upi/vsphere/main.tf
+++ upi/vsphere/main.tf
@@ -46,7 +46,7 @@ module "bootstrap" {
 module "control_plane" {
   source = "./machine"
 
-  name             = "control-plane"
+  name             = "master"
   instance_count   = "${var.control_plane_count}"
   ignition         = "${var.control_plane_ignition}"
   resource_pool_id = "${module.resource_pool.pool_id}"
@@ -65,7 +65,7 @@ module "control_plane" {
 module "compute" {
   source = "./machine"
 
-  name             = "compute"
+  name             = "worker"
   instance_count   = "${var.compute_count}"
   ignition         = "${var.compute_ignition}"
   resource_pool_id = "${module.resource_pool.pool_id}"
@@ -81,15 +81,3 @@ module "compute" {
   machine_cidr     = "${var.machine_cidr}"
 }
 
-module "dns" {
-  source = "./route53"
-
-  base_domain         = "${var.base_domain}"
-  cluster_domain      = "${var.cluster_domain}"
-  bootstrap_count     = "${var.bootstrap_complete ? 0 : 1}"
-  bootstrap_ips       = ["${module.bootstrap.ip_addresses}"]
-  control_plane_count = "${var.control_plane_count}"
-  control_plane_ips   = ["${module.control_plane.ip_addresses}"]
-  compute_count       = "${var.compute_count}"
-  compute_ips         = ["${module.compute.ip_addresses}"]
-}
