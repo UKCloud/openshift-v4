6e76e2d9c6d9fc62f69560b47bb556094fb91535
Steve Mulholland <smulholland@ukcloud.com>
2019-11-07 15:33:23 +0000

    adding step 3 code

---
+++ upi/vsphere/stage2/3.setup-bastion/setup-bastion.ps1
@@ -0,0 +1,34 @@
+###########################################################################
+# This generates ignition for the Bastion
+# and then creates and configures the VM
+###########################################################################
+# Inputs are from ./config.json,
+# username/passiword for vCenter is from ./secrets.json
+###########################################################################
+
+# Read in the configs
+$ClusterConfig = Get-Content -Raw -Path ../config.json | ConvertFrom-Json
+$SecretConfig = Get-Content -Raw -Path ../secrets.json | ConvertFrom-Json
+
+# New code required to create install-config file
+
+# Read vars from config file
+$global:base_domain = $ClusterConfig.basedomain
+$global:machinecidr = ($ClusterConfig.network.networkip + "/" + $ClusterConfig.network.maskprefix)
+$global:vcenterserver = $ClusterConfig.vsphere.vsphere_server 
+$global:vcenterdatacenter = $ClusterConfig.vsphere.vsphere_datacenter
+$global:vsandatastore = $ClusterConfig.vsphere.vsphere_datastore
+$global:sshpubkey = $ClusterConfig.sshpubkey
+
+# Read vars from secret file
+$global:vcenteruser = $SecretConfig.vcenterdeploy.username
+$global:vcenterpassword = $SecretConfig.vcenterdeploy.password
+$global:pullsecret = $SecretConfig.rhpullsecret
+
+# Invoke template to generate the file
+$installconfig = Invoke-EpsTemplate -Path ./install-config.tmpl
+
+Out-File -FilePath ../install-config.yaml -InputObject $installconfig
+
+write-host -ForegroundColor green "Created install-config file as required"
+
