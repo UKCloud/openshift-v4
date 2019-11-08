74daed20082496e229beedbc512e5ade20d49f77
Gareth Ellner <gellner@ukcloud.com>
2019-11-08 12:13:33 +0000

    First stable at ansible postdeploy container; need to add code to 3.setup-bastion to make the ansible-hosts file

---
+++ upi/vsphere/stage2/playbooks/entrypoint.sh
@@ -0,0 +1,2 @@
+ansible-playbook -i /tmp/workingdir/openshift-ansible-hosts /usr/local/playbooks/configure_svc_dns.yaml 
+ansible-playbook -i /tmp/workingdir/openshift-ansible-hosts /usr/local/playbooks/configure_ntp.yaml
