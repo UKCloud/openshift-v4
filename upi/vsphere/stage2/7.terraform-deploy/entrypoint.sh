98bcb4caea82e16ea4ffa6f22d6b3de343860567
Gareth Ellner <gellner@ukcloud.com>
2019-11-20 09:39:30 +0000

    Bring fixes from deployment back into github

--- upi/vsphere/stage2/7.terraform-deploy/entrypoint.sh
+++ upi/vsphere/stage2/7.terraform-deploy/entrypoint.sh
@@ -1,2 +1,2 @@
 cd /usr/share/terraform/
-terraform plan -out=/tmp/workingdir/terraform.plan -var-file=/tmp/workingdir/config.json -var-file=/tmp/workingdir/secrets.json  -state=/tmp/workingdir/terraform.tfstate -no-color 
+terraform apply -var-file=/tmp/workingdir/config.json -var-file=/tmp/workingdir/secrets.json  -state=/tmp/workingdir/terraform.tfstate -no-color -auto-approve
