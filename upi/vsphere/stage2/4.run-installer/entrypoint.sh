9f2094e49addc5600fc54f2de9281dba8186dd38
Gareth Ellner <gellner@ukcloud.com>
2019-12-23 11:49:06 +0000

    do correction

--- upi/vsphere/stage2/4.run-installer/entrypoint.sh
+++ upi/vsphere/stage2/4.run-installer/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-
+function get_config () {
+  # function for getting config:
+  # Eg:
+  # get_config "sshpubkey"
+  # get_config "svcs[0].hostname"
+  cat /tmp/workingdir/config.json | jq .$1
+}
 
 # Clear state so that new install CA is created
 mv .openshift_install_state.json .openshift_install_state.json.old
@@ -10,7 +16,14 @@ cp install-config.yaml .install-config.yaml.bak
 # Create manifests
 openshift-install create manifests
 
+# Substitute folder name
+CLUSTERID=$( get_config "clusterid" )
+FOLDERNAME=$( get_config "vsphere.vsphere_folder" )
+sed -i "s/folder            = ${CLUSTERID}/folder            = ${FOLDERNAME}/g" manifests/cloud-provider-config.yaml
+
 
 # Create ignition
-openshift-install create ignition-configs
-cp worker.ign infra.ign
+#openshift-install create ignition-configs
+#cp worker.ign infra.ign
+
+
