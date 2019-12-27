15a37f006cbca79b9d86216bf23bc60616e95417
Gareth Ellner <gellner@ukcloud.com>
2019-12-27 16:31:03 +0000

    Add script to finalise the installer

--- upi/vsphere/stage2/finalise.sh
+++ upi/vsphere/stage2/finalise.sh
@@ -2,7 +2,6 @@
 # Script to run finalise containers
 TAG=0.5
 
-
+sudo podman run --entrypoint="/usr/local/4.run-installer/waitforcomplete.sh" -v /home/core/deployconfig:/tmp/workingdir:z 4.run-installer:${TAG}
 sudo podman run -v ~/deployconfig:/tmp/workingdir:z 9.finalise-install:${TAG}
-
 sudo podman run --entrypoint="./removebootstrap.sh" -v ~/deployconfig:/tmp/workingdir:z 7.terraform-deploy:${TAG}
