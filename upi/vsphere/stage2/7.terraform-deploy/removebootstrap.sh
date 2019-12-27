beeb8ab04075cd8c6d9b0dbfb6c70061e960c338
Gareth Ellner <gellner@ukcloud.com>
2019-12-27 13:20:31 +0000

    Add entrypoint script for deleting bootstrap VM

---
+++ upi/vsphere/stage2/7.terraform-deploy/removebootstrap.sh
@@ -0,0 +1,4 @@
+cd /usr/share/terraform/
+sed -i '/clusterid/a \
+    "bootstrap_complete": "true",' /tmp/workingdir/config.json
+terraform apply -var-file=/tmp/workingdir/config.json -var-file=/tmp/workingdir/secrets.json  -state=/tmp/workingdir/terraform.tfstate -no-color -auto-approve
