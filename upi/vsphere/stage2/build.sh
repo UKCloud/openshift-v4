7cda17abae66d77c3f8735ff5852e473bbd6262a
Gareth Ellner <gellner@ukcloud.com>
2020-01-06 16:17:29 +0000

    Remove staging and .apps. prefix! !

--- upi/vsphere/stage2/build.sh
+++ upi/vsphere/stage2/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Script to locally build stage2 containers
 echo "Enter the image tag version to build:"
-TAG=$(read)
+read TAG
 
-sudo podman build ./3.setup-bastion -t 3.setup-bastion:${TAG} --no-cache
-sudo podman build ./4.run-installer -t 4.run-installer:${TAG} --no-cache
-sudo podman build ./5.ign-webserver -t 5.ign-webserver:${TAG} --no-cache
-sudo podman build ./6.add-ignition -t 6.add-ignition:${TAG} --no-cache
-sudo podman build ./7.terraform-deploy -t 7.terraform-deploy:${TAG} --no-cache
-sudo podman build ./8.post-deployment -t 8.post-deployment:${TAG} --no-cache
-sudo podman build ./9.finalise-install -t 9.finalise-install:${TAG} --no-cache
+
+
+sudo podman build ./3.setup-bastion -t ${PREFIX}/3.setup-bastion:${TAG} --no-cache
+sudo podman build ./4.run-installer -t ${PREFIX}/4.run-installer:${TAG} --no-cache
+sudo podman build ./5.ign-webserver -t ${PREFIX}/5.ign-webserver:${TAG} --no-cache
+sudo podman build ./6.add-ignition -t ${PREFIX}/6.add-ignition:${TAG} --no-cache
+sudo podman build ./7.terraform-deploy -t ${PREFIX}/7.terraform-deploy:${TAG} --no-cache
+sudo podman build ./8.post-deployment -t ${PREFIX}/8.post-deployment:${TAG} --no-cache
+sudo podman build ./9.finalise-install -t ${PREFIX}/9.finalise-install:${TAG} --no-cache
