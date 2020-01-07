73d600fd3348a9bc9b96cb60e9cf11044a91c00d
Gareth Ellner <gellner@ukcloud.com>
2020-01-07 09:45:05 +0000

    Add alteration to domain in ingress config

--- upi/vsphere/stage2/4.run-installer/entrypoint.sh
+++ upi/vsphere/stage2/4.run-installer/entrypoint.sh
@@ -24,6 +24,13 @@ CLUSTERID=$( get_config "clusterid" | sed 's/"//g' )
 FOLDERNAME=$( get_config "vsphere.vsphere_folder" | sed 's/"//g' )
 sed -i "s/folder            = ${CLUSTERID}/folder            = ${FOLDERNAME}/g" manifests/cloud-provider-config.yaml
 
+# Remove apps. prefix
+sed -i "s/apps.//g" manifests/cluster-ingress-02-config.yml
+
+echo "This is the manifest for ingress:"
+cat manifests/cluster-ingress-02-config.yml
+
+
 # Create ignition
 openshift-install create ignition-configs
 cp worker.ign infra.ign
