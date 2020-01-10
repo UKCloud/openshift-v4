83a1abb7b8585df84117fec3facb0eeadf1dbf4e
Gareth Ellner <gellner@ukcloud.com>
2020-01-10 09:51:44 +0000

    Add automatic installation of certs - LE is still staging

--- letsencrypt/entrypoint.sh
+++ letsencrypt/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 ansible-playbook -i /root/ansible-hosts /usr/local/letsencrypt/acme.sh/deployment.yml
+ansible-playbook -vvv -i /root/ansible-hosts /usr/local/letsencrypt/replace_certificates.yml
