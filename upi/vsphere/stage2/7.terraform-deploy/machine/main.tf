b028cecbf1b30eb694ae3daa4da8d3aae64afb4d
Gareth Ellner <gellner@ukcloud.com>
2020-02-12 14:58:37 +0000

    try another conditional

--- upi/vsphere/stage2/7.terraform-deploy/machine/main.tf
+++ upi/vsphere/stage2/7.terraform-deploy/machine/main.tf
@@ -40,7 +40,7 @@ resource "vsphere_virtual_machine" "vm" {
   }
 
   dynamic "network_interface" {
-    for_each = var.transit_network =! "" ? [var.transit_network] : []
+    for_each = "${var.transit_network =! "" ? [var.transit_network] : [] }"
     content {
       network_id = data.vsphere_network.transit_network.id
     }
