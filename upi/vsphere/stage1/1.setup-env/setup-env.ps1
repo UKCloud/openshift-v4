009ef403021a2b7d81280abf73b0a9720adcdf3e
Gareth Ellner <gellner@ukcloud.com>
2020-03-18 17:45:14 +0000

    Add diag messages

--- upi/vsphere/stage1/1.setup-env/setup-env.ps1
+++ upi/vsphere/stage1/1.setup-env/setup-env.ps1
@@ -171,6 +171,7 @@ function Add-App-LB {
   try {
       Write-Output -InputObject "About to get tcpMonitor **********************"
       $tcpMonitor = $edge | Get-NsxLoadBalancer | Get-NsxLoadBalancerMonitor -Name default_tcp_monitor
+      write-host -ForegroundColor cyan "Inside Monitor object: " ($tcpMonitor | Format-Table | Out-String)
   }
   catch {
       Write-Error -Message "The monitor: default_tcp_monitor not found. Attempting to create it..."
