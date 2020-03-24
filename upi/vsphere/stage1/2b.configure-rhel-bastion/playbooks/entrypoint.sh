80d90d961d68cc83af81000a66218e01402d3b89
Gareth Ellner <gellner@ukcloud.com>
2020-03-24 13:41:35 +0000

    Add some broken code to make RHEL bastion

---
+++ upi/vsphere/stage1/2b.configure-rhel-bastion/playbooks/entrypoint.sh
@@ -0,0 +1,3 @@
+export ANSIBLE_SSH_RETRIES=8
+ansible-playbook -i /tmp/workingdir/ansible-hosts /usr/local/playbooks/install_key_rhel.yaml
+ansible-playbook -i /tmp/workingdir/ansible-hosts /usr/local/playbooks/init_rhel.yaml
