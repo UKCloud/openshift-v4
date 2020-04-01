0ba6e2728059f743e265978b03d23a1c888b415c
Gareth Ellner <gellner@ukcloud.com>
2020-04-01 16:04:28 +0100

    Add seperate bastion playbook and do yum update

--- upi/vsphere/stage1/3b.configure-rhel-bastion/playbooks/entrypoint.sh
+++ upi/vsphere/stage1/3b.configure-rhel-bastion/playbooks/entrypoint.sh
@@ -4,3 +4,4 @@ echo "************ Using Port 80 for SSH! ************"
 echo "************ Requirement - Bastion must be accessible via SSH on the mangement.externalvip in config.json from this host ************"
 ansible-playbook -i /tmp/workingdir/ansible-hosts /usr/local/playbooks/install_key_rhel.yaml
 ansible-playbook -i /tmp/workingdir/ansible-hosts /usr/local/playbooks/init_rhel.yaml
+ansible-playbook -i /tmp/workingdir/ansible-hosts /usr/local/playbooks/init_bastion.yaml
