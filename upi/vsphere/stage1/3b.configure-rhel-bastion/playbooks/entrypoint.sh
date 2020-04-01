25ef49721d17bb8fcf849196fc98392c62b41c76
Gareth Ellner <gellner@ukcloud.com>
2020-04-01 11:21:00 +0100

    Perverse hack - use Port 80 for SSH!

--- upi/vsphere/stage1/3b.configure-rhel-bastion/playbooks/entrypoint.sh
+++ upi/vsphere/stage1/3b.configure-rhel-bastion/playbooks/entrypoint.sh
@@ -1,7 +1,9 @@
-export ANSIBLE_SSH_RETRIES=8
+export ANSIBLE_SSH_RETRIES=2
 
 ## Hack to allow testing in Pod8
 export ANSIBLE_PORT=80
+export ANSIBLE_SSH_PORT=80
+
 echo "************ Using Port 80 for SSH! ************"
 echo "************ Requirement - Bastion must be accessible via SSH on the mangement.externalvip in config.json from this host ************"
 ansible-playbook -i /tmp/workingdir/ansible-hosts /usr/local/playbooks/install_key_rhel.yaml
