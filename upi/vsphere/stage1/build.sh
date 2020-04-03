a845901baa543d6a74bd2eb9b3d720abec6dd296
Gareth Ellner <gellner@ukcloud.com>
2020-04-03 12:22:02 +0100

    Add coredns retag/push to stage1 build

--- upi/vsphere/stage1/build.sh
+++ upi/vsphere/stage1/build.sh
@@ -12,6 +12,7 @@ podman build ./3a.create-rhel-bastion -t ${PREFIX}/3a.create-rhel-bastion:${TAG}
 podman build ./3b.configure-rhel-bastion -t ${PREFIX}/3b.configure-rhel-bastion:${TAG} --no-cache
 podman build ./9.finalise-install -t ${PREFIX}/9.finalise-install:${TAG} --no-cache
 
+podman tag docker.io/coredns/coredns:latest ${PREFIX}/coredns:${TAG}
 
 read -p "Press [Enter] to push images to registry, or [Ctrl-C] to cancel"
 
@@ -20,4 +21,5 @@ podman push --tls-verify=false ${PREFIX}/2.create-config:${TAG}
 podman push --tls-verify=false ${PREFIX}/3a.create-rhel-bastion:${TAG}
 podman push --tls-verify=false ${PREFIX}/3b.configure-rhel-bastion:${TAG}
 podman push --tls-verify=false ${PREFIX}/9.finalise-install:${TAG}
+podman push --tls-verify=false ${PREFIX}/coredns:${TAG}
 
