d8030049361dda5eb87091a657bce5706f0fd26e
Gareth Ellner <29887711+gellner@users.noreply.github.com>
2020-07-15 12:11:07 +0100

    modify run-installer for 4.4/4.5

--- upi/vsphere/stage2/4.run-installer/entrypoint.sh
+++ upi/vsphere/stage2/4.run-installer/entrypoint.sh
@@ -26,16 +26,7 @@ fi
 # Create manifests
 $INSTALLCOMMAND create manifests
 
-# Substitute folder name
-CLUSTERID=$( get_config "clusterid" | sed 's/"//g' )
-FOLDERNAME=$( get_config "vsphere.vsphere_folder" | sed 's/"//g' )
-
-echo "Cloud provider config before:"
-cat manifests/cloud-provider-config.yaml
-sed -i "s/folder            = ${CLUSTERID}/folder            = ${FOLDERNAME}/g" manifests/cloud-provider-config.yaml
-
-echo "\n\nCloud provider config after edit:"
-cat manifests/cloud-provider-config.yaml
+rm -f openshift/99_openshift-cluster-api_master-machines-*.yaml openshift/99_openshift-cluster-api_worker-machineset-*.yaml
 
 # Create ignition
 $INSTALLCOMMAND create ignition-configs
