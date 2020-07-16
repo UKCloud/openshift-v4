7d2726ade117f3cc606dc17b6eceadbf70928775
Gareth Ellner <29887711+gellner@users.noreply.github.com>
2020-07-16 12:10:22 +0100

    Changes before finalising 4.5 code for Pod17

--- upi/vsphere/stage1/build.sh
+++ upi/vsphere/stage1/build.sh
@@ -10,7 +10,6 @@ podman build ./1.setup-env -t ${PREFIX}/1.setup-env:${TAG} --no-cache
 podman build --format=docker ./2.create-config -t ${PREFIX}/2.create-config:${TAG} --no-cache
 podman build ./3a.create-rhel-bastion -t ${PREFIX}/3a.create-rhel-bastion:${TAG} --no-cache
 podman build ./3b.configure-rhel-bastion -t ${PREFIX}/3b.configure-rhel-bastion:${TAG} --no-cache
-#podman build ./99.finalise-install -t ${PREFIX}/99.finalise-install:${TAG} --no-cache
 
 podman tag docker.io/coredns/coredns:latest ${PREFIX}/coredns:${TAG}
 
@@ -20,6 +19,5 @@ podman push --tls-verify=false ${PREFIX}/1.setup-env:${TAG}
 podman push --tls-verify=false ${PREFIX}/2.create-config:${TAG}
 podman push --tls-verify=false ${PREFIX}/3a.create-rhel-bastion:${TAG}
 podman push --tls-verify=false ${PREFIX}/3b.configure-rhel-bastion:${TAG}
-#podman push --tls-verify=false ${PREFIX}/99.finalise-install:${TAG}
 podman push --tls-verify=false ${PREFIX}/coredns:${TAG}
 
