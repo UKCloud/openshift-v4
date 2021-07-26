78e3fb591d28ea422622a0b562f843f348becef1
Ben Bacon <benbacon@users.noreply.github.com>
2021-07-26 16:15:18 +0100

    Net2 ingress (#62)
    * Initial commit of net2-ingress Ansible runner
    
    * Add missing hash of latest ansible-runner image
    
    * Handle net2 nodes not existing
    
    * Add net2ingress SA RBAC playbook
    
    * Bump versions to latest and fix various credential/permissions issues
    
    * Add net2 IngressController
    
    * Add build nodeselector and make idempotent
    
    * Change object name prefix from net2 to ukc
    
    * Creation of CronJob to update IngressController replicas based on backing nodes
    
    * Replace spaces with hyphens in net2-ingress playbook
    
    Co-authored-by: gellner <29887711+gellner@users.noreply.github.com>
    
    * Perform replace of spaces/underscores within var definition
    
    Co-authored-by: gellner <29887711+gellner@users.noreply.github.com>

---
+++ post-deployment/openstack/net2-ingress/build/playbooks/entrypoint.sh
@@ -0,0 +1,10 @@
+export K8S_AUTH_VERIFY_SSL=no
+
+# Add uid to /etc/passwd to allow Ansible to run
+if [ `id -u` -ge 500 ]; then
+    echo "runner:x:`id -u`:`id -g`:,,,:/runner:/bin/bash" > /tmp/passwd
+    cat /tmp/passwd >> /etc/passwd
+    rm /tmp/passwd
+fi
+
+ansible-playbook /usr/local/playbooks/update_loadbalancer_members.yaml
